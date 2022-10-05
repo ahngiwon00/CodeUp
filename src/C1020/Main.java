259b7056274a2c47bef2e34186ee037f5af4337e
ahngiwon00 <ahngiwon00@naver.com>
2022-10-05 17:10:13 +0900

    코드업 기초100제 1011~1020

---
+++ src/C1020/Main.java
@@ -0,0 +1,14 @@
+package C1020;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String[] str= s.split("\\-");
+        for(String x : str)
+            System.out.print(x);
+    }
+}
+
