0fd7e19286c98400e5904950b533c7651de54781
ahngiwon00 <ahngiwon00@naver.com>
2022-10-11 18:44:44 +0900

    코드업 기초100제 1041~1050

---
+++ src/C1044/Main.java
@@ -0,0 +1,12 @@
+package C1044;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a= sc.nextLong();
+        System.out.println((long)a+1);
+    }
+}
+
