a0ef333bbe31dc06c638904e982f07170e6b996d
ahngiwon00 <ahngiwon00@naver.com>
2022-10-13 00:07:20 +0900

    1051~1060번 , c4596 코드업

--- src/C4596/Main.java
+++ src/C4596/Main.java
@@ -5,31 +5,30 @@ import java.util.Scanner;
 public class Main {
     public void solution(int[][] a){
 
-        int max=a[0][0];
-        int x=1;
-        int y=1;
-        for(int i=0; i<a.length; i++){
-            for(int j=0; j<a[i].length; j++){
-                if(max<a[i][j]){
-                    max=a[i][j];
-                    x=i+1;
-                    y=j+1;
+        int max = a[0][0];
+        int x = 1;
+        int y = 1;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (max < a[i][j]) {
+                    max = a[i][j];
+                    x = i + 1;
+                    y = j + 1;
                 }
             }
         }
 
         System.out.println(max);
-        System.out.println(x+" "+y);
+        System.out.println(x + " " + y);
     }
 
     public static void main(String[] args) {
-        Main main =new Main();
-
+        Main main = new Main();
         Scanner sc = new Scanner(System.in);
-        int[][] a= new int[9][9];
-        for(int i=0; i<a.length; i++){
-            for(int j=0; j<a[i].length; j++){
-                a[i][j]=sc.nextInt();
+        int[][] a = new int[9][9];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                a[i][j] = sc.nextInt();
             }
         }
 
